a65c5742adef4deaf4a84a1b3a199eebdadc3abc
zaki bousmaha <zakib9@gmail.com>
2024-08-30 18:29:49 +0100

    header
    adding assets theming & create the navbar and the header

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
+  <div class="flex flex-col text-xl">
+    <h1>This is an about page</h1>
     <h1>This is an about page</h1>
   </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,17 @@
 </script>
 
 <template>
-  <main>
-    
-  </main>
+  <div class="h-screen items-center justify-center flex mt-[-350px] bg-gradient-to-b from-primary via-secondary to-transparent">
+    <div class="flex flex-col gap-8 text-start flex-wrap max-w-fit mt-[500px] font-Karla font-bold text-8xl text-gray-700">
+      <h1 class=" text-gray-500">HI!</h1>
+      <h1>I am Zaki</h1>
+      <h1>Bousmaha</h1>
+      <div class="flex flex-col gap-2 text-4xl font-Karla font-extralight uppercase text-gray-500">
+        <h2>Frontend Web</h2>
+        <h2>Designer</h2>
+      </div>
+      <div class="flex flex-wrap max-w-fit bg-primary text-white rounded-full text-base font-light px-4 py-2 justify-center">Visit My Works</div>
+    </div>
+  </div>
+  
 </template>
