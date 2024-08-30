4d9dd49bc0f6fb33b391a6bffad65df1e410d5a0
zaki bousmaha <zakib9@gmail.com>
2024-08-30 12:53:22 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# meyawo
+ self website
---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <header>
+    <div class="text-center">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+
+
+
