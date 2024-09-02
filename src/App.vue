bd48edaa87e5917940c1aa4dbc21505c560e9734
zaki bousmaha <zakib9@gmail.com>
2024-09-02 17:07:01 +0100

    homeView
    complete home view design

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,30 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { ref, onMounted, onUnmounted } from 'vue'
+
+const scrolled = ref(false)
+
+const handleScroll = () => {
+  scrolled.value = window.scrollY > 50
+}
+
+onMounted(() => {
+  window.addEventListener('scroll', handleScroll)
+})
+
+onUnmounted(() => {
+  window.removeEventListener('scroll', handleScroll)
+})
+
+
+
+
 
 </script>
 
 <template>
-    <div class="flex gap-4 justify-between m-auto bg-transparent py-6 px-80 text-xl text-white sticky top-0">
-        <h1 class="text-4xl text-white font-Karla font-extrabold">Meyawo</h1>
+    <div :class="['flex gap-4 justify-between px-80 py-5 text-xl fixed m-auto w-full top-0 left-0 transition-colors duration-300', { 'bg-white shadow-md text-primary': scrolled, 'bg-transparent text-white': !scrolled }]"
+      >
+        <h1 class="text-4xl font-Karla font-extrabold">Meyawo</h1>
         <div class="flex gap-4 justify-center items-center font-Karla font-thin">
           <RouterLink to="/">Home</RouterLink>
           <p>About</p>
@@ -13,12 +32,11 @@ import { RouterLink, RouterView } from 'vue-router'
           <p>Testmonial</p>
           <p>Blog</p>
           <p>Contact</p>
-          <RouterLink to="/about" class="p-2 px-4 bg-primary rounded-sm">Components</RouterLink>
+          <RouterLink to="/about" class="p-2 px-4 bg-primary text-white rounded-sm">Components</RouterLink>
           <svg xmlns="http://www.w3.org/2000/svg" height="32" width="24" viewBox="0 0 384 512"><path fill="#ffffff"  opacity="0.8" d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>
         </div>
-
-        
     </div>
+  
     
   <RouterView />
 </template>
